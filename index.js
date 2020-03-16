76cd7fff8ced05607c21e8225b9fbbbffe638f2d
Digvijay <digvijaybaloch@gmail.com>
2020-03-16 15:18:16 +0530

    changed greetings

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send({ bye: "buddy helloo champ" });
+	res.send({ hi: "champ" });
 });
 
 const PORT = process.env.PORT || 5000;
