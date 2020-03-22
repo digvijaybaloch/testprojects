34caa6e44a93cd21ddf4fb400f37ba0d67b40351
Digvijay <digvijaybaloch@gmail.com>
2020-03-22 22:19:26 +0530

    greetings changed

--- services/passport.js
+++ services/passport.js
@@ -13,14 +13,14 @@ passport.use(
 		},
 		(accessToken, refresToken, profile, done) => {
 			console.log(profile);
-			//course me ye tha but ye kaam nahi kr raha tha
-			// new User({ googleId: profile.id }).save();
-			const instance = new User({ googleId: profile.id });
-			instance.save(function(err) {
-				if (err) return handleError(err);
-				// saved!
-			});
-			console.log(instance);
+			new User({ googleId: profile.id }).save();
+			// const instance = new User({ googleId: profile.id });
+			// instance.save(function(err) {
+			// 	if (err) return handleError(err);
+			// 	// saved!
+			// });
+			// console.log(instance);
+			console.log(accessToken);
 		}
 	)
 );
